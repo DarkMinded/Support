c6be4195beddf2819c3e549e2eaedb114460b542
DarkMinded <nanjos@live.com>
2017-11-24 16:19:34 +0000

    First update
    db and stuf

---
+++ AcinetSuporte/Areas/AdminTables/AdminTablesAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AcinetSuporte.Areas.AdminTables
+{
+    public class AdminTablesAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "AdminTables";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "AdminTables_default",
+                "AdminTables/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ AcinetSuporte/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AcinetSuporte.MvcApplication" Language="C#" %>
